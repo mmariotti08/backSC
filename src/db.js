42bcabb978c0184e7030def381439159ed4b6ecb
unknown <brayanimbachi84@gmail.com>
2023-07-05 08:15:40 -0700

    nt

--- src/db.js
+++ src/db.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const {
+	DB_USER, DB_PASSWORD, DB_HOST,
+  } = process.env;
+  
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/products`, {
     logging: false,
