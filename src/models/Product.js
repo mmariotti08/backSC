83e0784d2f7fe3dcb2b2f4fde97fa1d9f24bfd3c
MRubilarRiffo <m.rubilar1010@gmail.com>
2023-07-05 00:23:04 -0400

    Agregada tabla Stock

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,40 +1,40 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  sequelize.define('Product', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-    },
-    brand_name: {
-      type: DataTypes.STRING,
-    },
-    category: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
-    },
-    color: {
-      type: DataTypes.STRING,
-    },
-    gender: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
-    },
-    main_picture_url: {
-      type: DataTypes.STRING,
-    },
-    retail_price_cents: {
-      type: DataTypes.INTEGER,
-    },
-    slug: {
-      type: DataTypes.STRING,
-    },
-    status: {
-      type: DataTypes.STRING,
-    },
-  }, {
-    timestamps: false,
-  });
+	sequelize.define('Product', {
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+		},
+		brand_name: {
+			type: DataTypes.STRING,
+		},
+		category: {
+			type: DataTypes.ARRAY(DataTypes.STRING),
+		},
+		color: {
+			type: DataTypes.STRING,
+		},
+		gender: {
+			type: DataTypes.ARRAY(DataTypes.STRING),
+		},
+		main_picture_url: {
+			type: DataTypes.STRING,
+		},
+		retail_price_cents: {
+			type: DataTypes.INTEGER,
+		},
+		slug: {
+			type: DataTypes.STRING,
+		},
+		status: {
+			type: DataTypes.STRING,
+		},
+	}, {
+		timestamps: false,
+	});
 };
