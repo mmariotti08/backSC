87807c4893c695bdd7fe4f4c589c6d57bd536141
MRubilarRiffo <m.rubilar1010@gmail.com>
2023-07-08 19:01:45 -0400

    Fix createProduct/Add getStock

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const productsRoutes = require('./productsRoutes');
+const stocksRoutes = require('./stocksRoutes');
 
 const router = Router();
 
-router.use('/products', productsRoutes)
+router.use('/products', productsRoutes);
+
+router.use('/stocks', stocksRoutes);
 
 module.exports = router;
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,6 +1,16 @@
 const productsRoutes = require('express').Router();
+<<<<<<< Updated upstream
 const { getproducts } = require('../controllers/getproducts');
 
 productsRoutes.get('/', getproducts);
+=======
+const { getProducts } = require('../controllers/getProducts');
+const { getProductsID } =require('../controllers/getProductsID');
+const { createProduct }= require('../controllers/createProduct');
+
+productsRoutes.get('/', getProducts);
+productsRoutes.get('/:id', getProductsID);
+productsRoutes.post('/', createProduct);
+>>>>>>> Stashed changes
 
 module.exports = productsRoutes;
