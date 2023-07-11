3ba3fed7c9bba468824a9153b334e754637b2084
Jose Centeno <josemanuelcm25@gmail.com>
2023-07-11 16:26:31 -0300

    ruta delete y push

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -3,11 +3,13 @@ const { getProducts } = require('../controllers/getProducts');
 const { getProductsID } =require('../controllers/getProductsID')
 const {createProduct}= require('../controllers/createProduct')
 const {updateProduct}= require('../controllers/updateProduct')
+const {deleteProduct}= require('../controllers/deleteProduct')
 
 productsRoutes.get('/', getProducts);
 productsRoutes.get('/:id', getProductsID);
 productsRoutes.post('/', createProduct );
-productsRoutes.put('/:id', updateProduct)
+productsRoutes.put('/:id', updateProduct);
+productsRoutes.delete('/:id', deleteProduct);
 
 
 module.exports = productsRoutes;
