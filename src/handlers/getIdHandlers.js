a96e3e71db2d0fe0226049c0642960ca83949c6f
mmariotti08 <mariottimatias08@gmail.com>
2023-07-12 11:04:58 -0300

    bakeeend

--- src/handlers/getIdHandlers.js
+++ src/handlers/getIdHandlers.js
@@ -1,8 +1,14 @@
-const { Product} = require('../db');
+const { Product, Stock } = require('../db');
 
 const getIdHandler=async(id)=>{
     try {
-        const responseDb = await Product.findByPk(id);
+        const responseDb = await Product.findByPk(id,{ 
+            include: [
+            {
+                model: Stock,
+                attributes: ['size', 'quantity']
+            }
+        ]});
 
         if (!responseDb){
             return {error: `the shoes num ${id} not found `}
