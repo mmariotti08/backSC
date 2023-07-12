18abc583486467f19f133c479d6e6cecf416ef91
luissanchez92 <luisjosanchez69@gmail.com>
2023-07-12 13:02:18 -0300

    add-put-user

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,10 @@
 const userRoutes = require('express').Router();
 const {createUserControllers}=require('../controllers/userControllers/createUserControllers');
+const {updateUserControllers}=require('../controllers/userControllers/updateUserControllers')
 
 userRoutes.post('/',createUserControllers)
+
+userRoutes.put('/:id', updateUserControllers)
 
 
 module.exports=userRoutes;
