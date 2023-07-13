049e3139e8ebfa026ccca5a7b1026356fa035e6f
luissanchez92 <luisjosanchez69@gmail.com>
2023-07-13 11:25:24 -0300

    encriptado-password

--- src/controllers/userControllers/createUserControllers.js
+++ src/controllers/userControllers/createUserControllers.js
@@ -1,15 +1,14 @@
 const {createUserHandlers}=require('../../handlers/userHandlers/createUserHandlers')
+const {encryptPassword, compare}=require('../../helpers/helpers')
 
 const createUserControllers=async(req,res)=>{
-
-    const {name, mail, password, phone, last_name}=req.body
-    
-
     try{
+        const {name, mail, password, phone, last_name}=req.body
+        if (!name|| !mail || !password) throw Error('missing data for registration')
 
-        if (!name || !mail) throw Error('missing data for registration')
+        const passwordHash=await encryptPassword(password)
 
-        const response= await createUserHandlers(name, mail, password, phone, last_name)
+        const response= await createUserHandlers({name, mail, password: passwordHash , phone, last_name})
 
         response.error
         ? res.status(400).send(response.error)
