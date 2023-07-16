d1de6574275f4c9b464c7d52091231b6fcfef5e6
luissanchez92 <luisjosanchez69@gmail.com>
2023-07-16 20:51:03 -0300

    clean-paswword

--- src/handlers/userHandlers/createUserHandlers.js
+++ src/handlers/userHandlers/createUserHandlers.js
@@ -9,7 +9,14 @@ const createUserHandlers=async({name, mail, password, phone, last_name, address}
         })
 
         if (create){
-            return {message: 'new user registered successfully', user}
+            const userData={
+                id: user.id,
+                name: user.name,
+                last_name: user.name,
+                mail: user.mail,
+                phone: user.phone
+            }
+            return {message: 'new user registered successfully', userData}
         }else{
             return {message: `email: ${mail} was already registered previously`}
 
