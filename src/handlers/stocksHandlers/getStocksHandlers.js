b5a6d87c75f96bab95e4648903977fb006af2271
luissanchez92 <luisjosanchez69@gmail.com>
2023-07-23 23:56:25 -0300

    stock-sort

--- src/handlers/stocksHandlers/getStocksHandlers.js
+++ src/handlers/stocksHandlers/getStocksHandlers.js
@@ -3,7 +3,11 @@ const { Stock } = require('../../db');
 const getStocksHandlers = async () => {
     try {
 
-        const stocks = await Stock.findAll();
+        const stocks = await Stock.findAll(
+            {
+                order: [['id', 'ASC']]
+            }
+        );
         
         if (stocks.length > 0) return stocks;
         else return {error: `Stocks not found`};
--- src/handlers/stocksHandlers/getStocksID_Handlers.js
+++ src/handlers/stocksHandlers/getStocksID_Handlers.js
@@ -1,12 +1,12 @@
 const { Stock } = require('../../db');
-const { Op } = require('sequelize');
 
 const getStocksID_Handlers = async (id) => {
     try {
         const stocks = await Stock.findAll({
             where: {
                 productId: id,
-            }
+            },
+            order: [['id', 'ASC']]
         });
 
         if (stocks.length > 0) return stocks;
